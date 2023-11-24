7bec802d6bb47636f9e1eb4625a95906e1d6a777
wiznikvibe <nikhilshetty439@gmail.com>
2023-11-24 20:19:06 +0530

    Training Pipeline

--- main.py
+++ main.py
@@ -1,35 +1,16 @@
-from src.entity import config_entity
-from src.logger import logging
-from src.exception import CustomException
-import pandas as pd 
-import os, sys
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
-from src.entity import artifact_entity
-from src.components.data_ingestion import DataIngestion
-from src.components.data_validation import DataValidation
-from src.components.data_transformation import DataTransformation
-from src.components.model_trainer import ModelTrainer
+from src.pipeline.training_pipeline import start_training_pipeline
 
-training_pipeline_config = config_entity.TrainingPipelineConfig()
 
-data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
-data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
 
-data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
-data_validation = DataValidation(data_validation_config=data_validation_config, data_ingestion_artifact=data_ingestion_artifact)
-data_validation_artifact = data_validation.initiate_data_validation()
-# print(data_validation_artifact)
 
-data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
-data_transformation = DataTransformation(data_transformation_config=data_transformation_config, data_ingestion_artifact=data_ingestion_artifact)
-data_transformation_artifact = data_transformation.initiate_data_transformation()
-# print(data_transformation_artifact)
 
-model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
-model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
-print(model_trainer.initiate_model_trainer())
+
+if __name__=='__main__':
+    result = start_training_pipeline()
+    print(result)
+
 
 
 
