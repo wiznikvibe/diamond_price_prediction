46fc945be236ca5253945de2cf395ab2b7a7a8a4
wiznikvibe <nikhilshetty439@gmail.com>
2023-12-03 22:18:32 +0530

    Build and Push

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -7,10 +7,8 @@ from src.exception import CustomException
 from src.entity import config_entity, artifact_entity
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression, Lasso, Ridge, ElasticNet
-from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
 
 class ModelTrainer:
 
@@ -22,40 +20,18 @@ class ModelTrainer:
         mse = mean_squared_error(predicted, true)
         mae = mean_absolute_error(predicted, true)
         r2 = r2_score(predicted, true)
+        print(f"Mean Squared Error: {mse}")
+        print(f"Mean Absolute Error: {mae}")
 
-        return mse, mae, r2 
+        return  r2 
 
-    def evaluate_models(self, X_train, y_train, X_test, y_test, models)-> pd.DataFrame: 
-
-        models_list = []
-        score_list = []
-
-        for model_name, model in models.items():
-            model.fit(X_train, y_train)
-            y_test_pred = model.predict(X_test)
-            
-            model_test_mse, model_test_mae, model_test_r2 = self.evaluate_regression(y_test_pred, y_test)
-
-            print(model_name)
-            models_list.append(model_name)
-            
-            print('----------------------------------')
-
-            print(f"{model_name}")
-            print('Model performance for Test set')
-            print('- Mean Squared Error (MSE): {:.4f}'.format(model_test_mse))
-            print('- Mean Absolute Error (MAE): {:.4f}'.format(model_test_mae))
-            print('- R-squared: {:.4f}'.format(model_test_r2 * 100))
-            
-            logging.info('- Mean Squared Error (MSE): {:.4f}'.format(model_test_mse))
-            logging.info('- Mean Absolute Error (MAE): {:.4f}'.format(model_test_mae))
-            logging.info('- R-squared: {:.4f}'.format(model_test_r2 * 100))
-
-            score_list.append(model_test_r2)
-            print('=='*20)
-        
-        report = pd.DataFrame(list(zip(models_list, score_list)), columns=['Model_Name', 'R2_Score']).sort_values(by=['R2_Score'], ascending=False)
-        return report
+    def train_model(self, X, y):
+        try:
+            model = GradientBoostingRegressor()
+            model.fit(X, y)
+            return model
+        except Exception as e:
+            raise CustomException(e, sys)
 
     def initiate_model_trainer(self)->artifact_entity.ModelTrainerArtifact:
         try:
@@ -65,32 +41,38 @@ class ModelTrainer:
             X_train, y_train = train_array[:,:-1], train_array[:,-1]
             X_test, y_test = test_array[:,:-1], test_array[:,-1]
 
-            
-            models = {
-                "GradientBoostingRegressor": GradientBoostingRegressor(),
-            } 
+            model = self.train_model(X=X_train,y=y_train)
+            logging.info(f"Model is trained on these features: {model.feature_importances_}")
 
-            model_report = self.evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test, models=models)
-            logging.info(model_report)
-            
-            best_model = model_report[model_report.R2_Score == model_report.R2_Score.max()]
-            print(f"Best Model:{best_model['Model_Name']} || Best Model Score: {best_model['R2_Score']}")
-            logging.info(f"Best Model:{best_model['Model_Name']} || Best Model Score: {best_model['R2_Score']}")
-            best_model_name = best_model['Model_Name'].values[0]
-            save_model = models[best_model_name] 
+            logging.info("Computing R2 Score for Train Data")
+            y_train_pred = model.predict(X_train)
+            r2_train_score = self.evaluate_regression(predicted=y_train_pred, true=y_train)
 
-            save_object(file_dir=self.model_trainer_config.model_obj_dir, obj=save_model)
+            logging.info("Computing R2 Score for Test Data")
+            y_test_pred = model.predict(X_test)
+            r2_test_score = self.evaluate_regression(predicted=y_test_pred, true=y_test)
+
+            logging.info(f"R2 Score for Train Data: {r2_train_score} || R2 Score for Test Data: {r2_test_score}")
+            print(f"R2 Score for Train Data: {r2_train_score} || R2 Score for Test Data: {r2_test_score}")
+
+            save_object(file_dir=self.model_trainer_config.model_obj_dir, obj=model)
 
             model_trainer_artifact = artifact_entity.ModelTrainerArtifact(
                 model_obj_dir=self.model_trainer_config.model_obj_dir
             )
 
             return model_trainer_artifact
-
-            
             
         except Exception as e:
             raise CustomException(e, sys)
+            
+            
+            
+            
+
+
+
+            
             
             
             
